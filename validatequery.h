64d23cc4a5e54279a17428261c5ef9e53121040d
SaadAliBaig84 <115585560+SaadAliBaig84@users.noreply.github.com>
2023-11-04 03:23:28 -0700

    Update validatequery.h

--- validatequery.h
+++ validatequery.h
@@ -1,6 +1,3 @@
-#ifndef VALIDATEQUERY_H
-#define VALIDATEQUERY_H
-
 #pragma once
 #include<iostream>
 #include<vector>
@@ -13,24 +10,40 @@
 class validateQuery
 {
 public:
-    validateQuery(int);
-    int numOfColumns;
-    bool validate(std::vector<std::string> v1);
-    std::pair<std::string, std::vector<std::pair<std::string, std::string>>> query;
-    //std::pair<std::string, std::vector<std::pair<std::string, std::string>>> insertQuery;
-    std::pair<std::string, std::vector < std::vector<std::pair<std::string, std::string> > > > InsertQuery;
-    std::map<std::string, std::string> keys;
-    std::pair< std::string, std::vector<std::string>> selectQuery;
-    //std::pair< std::string, std::vector<std::string>> furherSelectQuery;
-    std::vector<std::tuple<std::string, std::string, std::string>> whereSelect;
-    std::pair<std::string, std::string> orderSelect;
-    std::pair<std::pair<std::string, std::string>, std::pair<std::string, std::string>> joinSelect;
-    std::string deleteQuery;
-    std::pair<std::string, std::vector<std::pair<std::string, std::string>>> updateQuery;
-    std::pair<std::string, std::vector<std::pair<std::string, std::string>>> alterQuery;
-    std::string dropTableQuery;
+	int numOfColumns;
+	validateQuery(int);
+	bool validate(std::vector<std::string> v1);
+	std::pair<std::string, std::vector<std::pair<std::string, std::string>>> query;
+	bool createCheck;
+
+	std::pair<std::string, std::vector < std::vector<std::pair<std::string, std::string> > > > InsertQuery;
+	bool insertCheck;
+
+	std::map<std::string, std::string> keys;
+	bool pkeyCheck;
+	bool fkeyCheck;
+
+	std::pair< std::string, std::vector<std::string>> selectQuery;
+	bool selectCheck;
+
+	std::vector<std::tuple<std::string, std::string, std::string>> whereSelect;
+	bool whereCheck;
+	std::pair<std::string, std::string> orderSelect;
+	bool orderCheck;
+
+	std::pair<std::pair<std::string, std::string>, std::pair<std::string, std::string>> joinSelect;
+	bool joinCheck;
+	
+	std::string deleteQuery;
+	bool deleteCheck;
+	
+	std::pair<std::string, std::vector<std::pair<std::string, std::string>>> updateQuery;
+	bool updateCheck;
+	
+	std::pair<std::string, std::vector<std::pair<std::string, std::string>>> alterQuery;
+	bool alterCheck;
+	
+	std::string dropTableQuery;
+	bool dropTableCheck;
 };
 
-
-
-#endif // VALIDATEQUERY_H
